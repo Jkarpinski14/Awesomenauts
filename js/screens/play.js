7d176e4fa071c060218fe8e60599360685eff34e
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-01-22 08:29:23 -0800

    Videos 1-7 completed

---
+++ js/screens/play.js
@@ -0,0 +1,32 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+
+		me.levelDirector.loadLevel("level01");
+		//loads the level/map when the game is opened and melonJS is run
+
+		var player = me.pool.pull("player", 0, 420, {});
+		me.game.world.addChild(player, 5);
+		//pull instance of the player
+
+		me.input.bindKey(me.input.KEY.RIGHT, "right");
+		//binds a key for movement
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+});
