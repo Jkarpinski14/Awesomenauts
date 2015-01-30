9b21d49adece225cf94e456fd0b766011c5c16bf
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-01-29 23:24:32 -0800

    Comments for video 19 added

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -255,10 +255,14 @@ game.GameManager = Object.extend({
 		this.now = new Date().getTime();
 
 		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			//% (mod) checks to see if we have a multiple of 10
+			//makes u wait at least a second for the next creep to spawn
 			this.lastCreep = this.now;
+			//this.now is the timer
 			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
 			me.game.world.addChild(creepe, 5);
 		}
+		//keeps track of our timer and whether or not new creeps should be made
 
 		return true;
 	}
--- js/game.js
+++ js/game.js
@@ -45,7 +45,7 @@ var game = {
 		me.pool.register("EnemyBase", game.EnemyBaseEntity);
 		me.pool.register("EnemyCreep", game.EnemyCreep, true);
 		me.pool.register("GameManager", game.GameManager);
-
+		//set to true when there's multiple of that entity
 
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
