fdaa84f0da691f4ee149904afa6d9327c71ddb24
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-01-29 22:32:54 -0800

    Video 16 (collisions from above) completed

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -85,9 +85,15 @@ game.PlayerEntity = me.Entity.extend({
 			var ydif = this.pos.y - response.b.pos.y;
 			var xdif = this.pos.x - response.b.pos.x;
 			//response.b represents whatever we're colliding with
-			console.log("xdif" + xdif + "ydif" + ydif);
 			
-			if(xdif>-35 && this.facing==='right' && (xdif<0)){
+			console.log("xdif" + xdif + "ydif" + ydif);
+
+			if(ydif<-40 && xdif<70 && xdif>-35){
+				this.body.falling = false;
+				this.body.vel.y = -1;
+			}
+			
+			else if(xdif>-35 && this.facing==='right' && (xdif<0)){
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x - 1;
 			}
