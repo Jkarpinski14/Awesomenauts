5647416f574926f820284537edebd6268912d59f
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-02-18 20:38:10 -0800

    Video 23 (player can kill creeps) completed

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -122,6 +122,31 @@ game.PlayerEntity = me.Entity.extend({
 				response.b.loseHealth();
 			}
 			//checking to see if it's been 1000 milliseconds (1 second) since the base was hit, so the player doesn't keep attacking over and over again
+			else if(response.b.type==='EnemyCreep'){
+				var xdif = this.pos.x - response.b.pos.x;
+				var ydif = this.pos.y - response.b.pos.y;
+
+				if (xdif>0){
+					this.pos.x = this.pos.x + 1;
+					if(this.facing==="left"){
+						this.body.vel.x = 0;
+					}
+				}
+				else{
+					this.pos.x = this.pos.x - 1;
+					if(this.facing==="right"){
+						this.body.vel.x = 0;
+					} 
+				}
+
+				if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000
+					&& (Mat.abs(ydif) <=40) && 
+					((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right")
+					){
+					this.lastHit = this.now;
+					response.b.loseHealth(1);
+				}
+			}
 
 		}
 	}
@@ -253,7 +278,16 @@ game.EnemyCreep = me.Entity.extend({
 		this.renderable.setCurrentAnimation("walk");
 	},
 
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+	},
+
 	update: function(delta){
+		console.log(this.health);
+		if(this.health <= 0){
+			me.game.world.removeChild(this);
+		}
+
 		this.now = new Date().getTime();
 
 		this.body.vel.x -= this.body.accel.x * me.timer.tick;
