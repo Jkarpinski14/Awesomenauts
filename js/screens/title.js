e6da453c62ee1477425d1763d1202ef9d22bc634
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-02-23 10:43:13 -0800

    Video 27 (making the title screen functional) completed

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,13 +6,33 @@ game.TitleScreen = me.ScreenObject.extend({
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
 		//the -10 is the z-axis for putting our title screen in the back
 		//the above line of code adds an image to the title screen
+
+		me.input.bindKey(me.input.KEY.ENTER, "start");
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.height]);
+				this.font = new me.Font("Arial", 46, "white");
+			},
+
+			draw: function(){
+				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
+				this.font.draw(renderer.getContext(), "Press Enter To Play!", 250, 530);
+			}
+		})));
+
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+			if(action === "start"){
+				me.state.change(me.state.PLAY);
+			}
+		});
 	},
-	
+	 
 	
 	/**	
 	 *  action to perform when leaving this screen (state change)
 	 */
-	onDestroyEvent: function() {
-		; // TODO
+	onDestroyEvent: function(renderer) {
+		me.input.unbindKey(me.input.KEY.ENTER);
+		me.event.unsubscribe(this.handler);
 	}
 });
