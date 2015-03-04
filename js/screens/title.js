9d25faea75a619b987bfebcfdab8036e8b5fd816
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-03-04 08:54:00 -0800

    Video 40 (continue) completed

--- js/screens/title.js
+++ js/screens/title.js
@@ -38,6 +38,31 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				this.font = new me.Font("Arial", 54, "red");
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+				//"pointerdown" listens for the mouse to be clicked down
+				//"this" checks if you are clicking on the defined object
+			},
+
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+				//adds text to the title screen
+			},
+
+			update: function(dt){
+				return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				//"releasePointerEvent" makes sure the computer doesn't keep listening for the clicker event as the game goes on
+				me.state.change(me.state.PLAY);
+			}
+		})));
+
 	},
 	 
 	
