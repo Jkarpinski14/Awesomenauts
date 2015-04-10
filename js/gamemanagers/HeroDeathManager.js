c8bcda17a9f005683b59625732bd613128c0335a
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-04-09 23:06:27 -0700

    Additional comment for clarification added

--- js/gamemanagers/HeroDeathManager.js
+++ js/gamemanagers/HeroDeathManager.js
@@ -8,6 +8,7 @@ game.HeroDeathManager = Object.extend({
 			me.game.world.removeChild(game.data.player);
 			//line of code added to make the initial dead player disappear
 			me.game.world.removeChild(game.data.miniPlayer);
+			//makes the dot on the minimap reappear where the player respawns after death
 			me.state.current().resetPlayer(10, 0);
 			//line of code added to respawn a player when health reaches zero
 		}
