c3e33e03ce89675e1a0f87c165f6cf246a72df32
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-04-09 22:07:45 -0700

    Video I6 completed without errors

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,15 @@
 game.MiniMap = me.Entity.extend({
-
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap",
+			width: 223,
+			height: 281,
+			spritewidth: "223",
+			spriteheight: "281",
+			getShape: function(){
+				return (new me.Rect(0, 0, 223, 281)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+	}
 });
